46d4cb37f4b2449131a0d617ba7989d337f719b7
tunaytoksoz <tunaytoksoz@hotmail.com>
2023-05-30 13:53:40 +0300

    stacview added label

--- Sources/recognitionFw/Controllers/IbanController.swift
+++ Sources/recognitionFw/Controllers/IbanController.swift
@@ -110,6 +110,7 @@ public class IbanController: UIViewController{
         NSLayoutConstraint.activate([
             cameraButton.widthAnchor.constraint(equalToConstant: 100),
             libraryButton.widthAnchor.constraint(equalToConstant: 100),
+            libraryButton.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: -10),
             
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
