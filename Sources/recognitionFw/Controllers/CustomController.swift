17b9ecd1981831d2f641aae53c372510e97bccf0
tunaytoksoz <tunaytoksoz@hotmail.com>
2023-05-30 23:03:50 +0300

    updated cardvalidator

--- Sources/recognitionFw/Controllers/CustomController.swift
+++ Sources/recognitionFw/Controllers/CustomController.swift
@@ -116,6 +116,7 @@ public class CustomController: UIViewController{
         NSLayoutConstraint.activate([
             cameraButton.widthAnchor.constraint(equalToConstant: 120),
             libraryButton.widthAnchor.constraint(equalToConstant: 120),
+            libraryButton.bottomAnchor.constraint(equalTo: stackView.bottomAnchor, constant: -10),
             
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
